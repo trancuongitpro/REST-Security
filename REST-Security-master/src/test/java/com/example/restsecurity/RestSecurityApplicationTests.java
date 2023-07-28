310cbe65ab03e5b0f91af7fd19d89055cde81e02
ADMIN <trancuongitpro@gmail.com>
2023-07-28 15:06:54 +0700

    Initial commit

---
+++ REST-Security-master/src/test/java/com/example/restsecurity/RestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
